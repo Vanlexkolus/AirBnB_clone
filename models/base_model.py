f4dde8e5b063d3957b335c38dfe6f59fda11f18e
EnduranceApeh <apehendurance133@gmail.com>
2024-02-10 15:20:14 -0800

    Debbugged code in line 59 and 31

--- models/base_model.py
+++ models/base_model.py
@@ -19,8 +19,8 @@ class BaseModel:
         Initialization of public attributes
         """
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
         if kwargs:
             for key, value in kwargs.items():
@@ -47,7 +47,7 @@ class BaseModel:
         updates the public instance attribute updated_at
         with the current datetime
         """
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
