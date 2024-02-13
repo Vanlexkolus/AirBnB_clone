e61733b101c8c688f5a8848ab702989778020397
Alexander olamide Kolurejo <Kolurejoalexanderola@gmail.com>
2024-02-13 03:46:38 +0100

    added the user file but I haven't changed the permissions

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from base_model import BaseModel
+class User(BaseModel):
+    """
+    This is a class named user that inherits from
+    the class BaseMode, it will be used to create
+    Public class attributes below for Task 8
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
